939a8d48ca50ba02cd1067168edf0b33de2f6d48
ahmetacr <ahmetacar1111@gmail.com>
2023-05-19 10:24:59 +0300

    Import image files and create Card components of them

---
+++ src/components/cardsArr.js
@@ -0,0 +1,56 @@
+import Card from "./Card";
+import aatrox from "../assets/aatrox.jpg";
+import ahri from "../assets/ahri.jpg";
+import akali from "../assets/akali.jpg";
+import alistar from "../assets/alistar.jpg";
+import amumu from "../assets/amumu.jpg";
+import anivia from "../assets/anivia.jpg";
+import ashe from "../assets/ashe.jpg";
+import azir from "../assets/azir.jpg";
+import brand from "../assets/brand.jpg";
+import caitlyn from "../assets/caitlyn.jpg";
+import cassiopeia from "../assets/cassiopeia.jpg";
+import diana from "../assets/diana.jpg";
+import draven from "../assets/draven.jpg";
+import ezreal from "../assets/ezreal.jpg";
+import jax from "../assets/jax.jpg";
+import katarina from "../assets/katarina.jpg";
+import leeSin from "../assets/lee-sin.jpg";
+import nasus from "../assets/nasus.jpg";
+import nidalee from "../assets/nidalee.jpg";
+import nocturne from "../assets/nocturne.jpg";
+import shaco from "../assets/shaco.jpg";
+import vayne from "../assets/vayne.png.webp";
+import vi from "../assets/vi.jpg";
+import zed from "../assets/zed.png.webp";
+
+const cardsArr = [
+  <>
+    <Card alt="aatrox" src={aatrox} />
+    <Card alt="ahri" src={ahri} />
+    <Card alt="akali" src={akali} />
+    <Card alt="alistar" src={alistar} />
+    <Card alt="amumu" src={amumu} />
+    <Card alt="anivia" src={anivia} />
+    <Card alt="ashe" src={ashe} />
+    <Card alt="azir" src={azir} />
+    <Card alt="brand" src={brand} />
+    <Card alt="caitlyn" src={caitlyn} />
+    <Card alt="cassiopeia" src={cassiopeia} />
+    <Card alt="diana" src={diana} />
+    <Card alt="draven" src={draven} />
+    <Card alt="ezreal" src={ezreal} />
+    <Card alt="jax" src={jax} />
+    <Card alt="katarina" src={katarina} />
+    <Card alt="lee-sin" src={leeSin} />
+    <Card alt="nasus" src={nasus} />
+    <Card alt="nidalee" src={nidalee} />
+    <Card alt="nocturne" src={nocturne} />
+    <Card alt="shaco" src={shaco} />
+    <Card alt="vayne" src={vayne} />
+    <Card alt="vi" src={vi} />
+    <Card alt="zed" src={zed} />
+  </>,
+];
+
+export default cardsArr;
