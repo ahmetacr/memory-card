1612b410a74f48464aed6cd4aae44a1a5cc0a6b0
ahmetacr <ahmetacar1111@gmail.com>
2023-05-19 10:25:09 +0300

    basic footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import "../styles/Footer.css";
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <h1>by @ahmetacr 2023</h1>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
