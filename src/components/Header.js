3118969401ab0416857f8b1df741498ee3b26ffa
ahmetacr <ahmetacar1111@gmail.com>
2023-05-20 12:50:56 +0300

    add score and bestscore sections

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import "../styles/Header.css";
-const Header = () => {
+const Header = (props) => {
   return (
     <>
       <div className="header">
         <h1>Memory Cards</h1>
+        <div className="scoreBoard">
+          <h2>Score: {props.score}</h2>
+          <h2>Best Score: {props.bestScore}</h2>
+        </div>
       </div>
     </>
   );
