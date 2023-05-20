2f749454041619f141f6511b501c11a246c67783
ahmetacr <ahmetacar1111@gmail.com>
2023-05-20 12:49:29 +0300

    add score, bestScore states, create methods for scoring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import React, { useState } from "react";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import Header from "./components/Header";
 import "./styles/App.css";
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  const incrementScore = () => {
+    setScore((prevScore) => {
+      let newScore = prevScore + 1;
+      if (newScore > bestScore) {
+        setBestScore(newScore);
+      }
+      return newScore;
+    });
+  };
+
+  const gameOver = () => {
+    alert("Game Over");
+    setScore(0);
+  };
+
   return (
     <div className="App">
-      <Header />
-      <Main />
+      <Header score={score} bestScore={bestScore} />
+      <Main incrementScore={incrementScore} gameOver={gameOver} />
       <Footer />
     </div>
   );
