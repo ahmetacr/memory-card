22b32fa453feecf3bcfe724b4b73341ee182beb1
ahmetacr <ahmetacar1111@gmail.com>
2023-05-20 12:51:38 +0300

    implement the game logic, create states, updateArray

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Main.css";
 import cardsArr from "./cardsArr";
+import Card from "./Card";
 
-function Main() {
+function Main({ incrementScore, gameOver }) {
+  const [cardArr, setCardArr] = useState(cardsArr);
+
+  const click = (e) => {
+    const clickedCardName = e.target.alt;
+    let hasLost = false;
+
+    const updatedArr = cardArr.map((card) => {
+      if (card.name === clickedCardName) {
+        if (card.clicked === false) {
+          return { ...card, clicked: true };
+        } else {
+          hasLost = true;
+        }
+      }
+      return card;
+    });
+
+    if (hasLost) {
+      gameOver();
+    } else {
+      incrementScore();
+      setCardArr(shuffleArray(updatedArr));
+    }
+  };
+
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]; // swap elements
+    }
+    return array;
+  };
+
   return (
     <div className="mainContainer">
-      {cardsArr.map((card, index) => (
-        <React.Fragment key={index}>{card}</React.Fragment>
+      {cardArr.map((card, index) => (
+        <Card key={index} src={card.src} name={card.name} onClick={click} />
       ))}
     </div>
   );
